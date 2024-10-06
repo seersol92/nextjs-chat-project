693d892b19a0c2d75612285c1295763f16670219
hAdi <seersol92@users.noreply.github.com>
2024-10-06 20:19:30 +0500

    done

---
+++ src/components/Option.tsx
@@ -0,0 +1,53 @@
+import Image from "next/image";
+
+interface OptionsProps {
+  options: any[];
+  hasImages: boolean;
+  onOptionClick: (option: string) => void;
+}
+
+const Options: React.FC<OptionsProps> = ({
+  options,
+  hasImages,
+  onOptionClick,
+}) => {
+  return (
+    <div className="flex flex-wrap justify-center space-x-2">
+      {options.map((option, index) => (
+        <div
+          key={index}
+          className={`${
+            hasImages
+              ? "mb-4 flex w-1/4 flex-col items-center justify-center"
+              : ""
+          }`}
+        >
+          {hasImages && option.image ? (
+            <div
+              className="cursor-pointer"
+              onClick={() => onOptionClick(option.name)}
+            >
+              <Image
+                src={option.image}
+                alt={option.name}
+                width={50} // Adjust width as needed
+                height={50} // Adjust height as needed
+                className="object-contain"
+              />
+              <span className="mt-1 text-center">{option.name}</span>
+            </div>
+          ) : (
+            <button
+              className="mb-2   flex-1 rounded bg-blue-500 px-5 py-2 text-white hover:bg-blue-600"
+              onClick={() => onOptionClick(option)}
+            >
+              {option}
+            </button>
+          )}
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Options;
