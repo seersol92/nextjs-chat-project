827ae0882ecb9d3724e6b3aebd2a99dc272e6d3c
hAdi <seersol92@users.noreply.github.com>
2024-10-14 12:26:01 +0500

    done

--- src/app/api/message/route.ts
+++ src/app/api/message/route.ts
@@ -4,20 +4,6 @@ import { NextApiRequest } from "next";
 
 const prisma = new PrismaClient();
 // Handle GET requests to fetch all leads
-export async function GET() {
-  try {
-     const messages = await prisma.message.findMany();
-    return NextResponse.json(messages, { status: 200 });
-  } catch (error) {
-    console.log("Error fetching messages:", error);
-    return NextResponse.json(
-      { message: "Error fetching messages" },
-      { status: 500 },
-    );
-  }
-}
-
-
 export async function PUT(request: Request) {
   const { text, options, id } = await request.json();
   console.log(text, options);
@@ -30,5 +16,20 @@ export async function PUT(request: Request) {
   } catch (error) {
     console.error("Error updating message:", error);
     return NextResponse.json("Error updating message", { status: 500 });
+  }
+}
+
+
+
+export async function GET() {
+  try {
+     const messages = await prisma.message.findMany();
+    return NextResponse.json(messages, { status: 200 });
+  } catch (error) {
+    console.log("Error fetching messages:", error);
+    return NextResponse.json(
+      { message: "Error fetching messages" },
+      { status: 500 },
+    );
   }
 }
